151f60b23d46148d3de18751f88ccf903d729463
E004014 <E004014@CTLINDT-0133.cignitiglobal.com>
2020-05-02 12:49:28 +0530

    Adding project to git

---
+++ src/main/java/Automation/winAppDriver/TestJava.java
@@ -0,0 +1,78 @@
+package Automation.winAppDriver;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("System date "+addingOrSubtractingDays("","","SystemDate","M/d/yyyy",7));
+		System.out.println("System date "+addingOrSubtractingDays("","","SystemDate","M/d/yyyy",-8));
+		System.out.println("System date "+addingOrSubtractingDays("","","SystemDate","d/MMMM/yyyy",7));
+		System.out.println("System date "+addingOrSubtractingDays("","","SystemDate","d/MMMM/yyyy",-8));
+		
+		System.out.println("System date "+addingOrSubtractingDays("8/April/2020","d/MMMM/yyyy","","M/d/yyyy",7));
+		System.out.println("System date "+addingOrSubtractingDays("8/April/2020","d/MMMM/yyyy","","M/d/yyyy",-8));
+		System.out.println("System date "+addingOrSubtractingDays("8/April/2020","d/MMMM/yyyy","","d/MMMM/yyyy",7));
+		System.out.println("System date "+addingOrSubtractingDays("8/April/2020","d/MMMM/yyyy","","d/MMMM/yyyy",-8));
+
+	}
+	
+	public static String addingOrSubtractingDays(String inputDate,String inputDateFormat,String systemDate,String outputDateFormat,int i) 
+	{
+		String newDate = "";
+		try {
+			SimpleDateFormat sdf;
+			Calendar c = null;
+			
+			//Calculates using system date
+			if(!systemDate.isEmpty())
+			{
+			//Get system date
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("M/d/yyyy");
+			LocalDateTime now = LocalDateTime.now();
+			
+			//Adding system date to Calendar instance
+			sdf = new SimpleDateFormat("M/d/yyyy");
+			c = Calendar.getInstance();
+			try {
+				c.setTime(sdf.parse(dtf.format(now)));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			}
+			
+			//Calculates using given input date and format
+			if(systemDate.isEmpty())
+			{
+				//Adding given input date to Calendar instance
+				sdf = new SimpleDateFormat(inputDateFormat);
+				c = Calendar.getInstance();
+				try
+				{
+					c.setTime(sdf.parse(inputDate));
+				}
+				catch (ParseException e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+			//Add/subtract days
+			c.add(Calendar.DAY_OF_MONTH, i);
+			sdf = new SimpleDateFormat(outputDateFormat);
+			newDate = sdf.format(c.getTime());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return newDate;
+
+	}
+	
+
+}
